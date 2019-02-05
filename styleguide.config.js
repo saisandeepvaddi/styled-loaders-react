dda254cf67e7d13a2984be51cd51e1017a98084c
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2019-02-04 22:57:22 -0500

    new lock file

--- styleguide.config.js
+++ styleguide.config.js
@@ -7,9 +7,9 @@ module.exports = {
                 {
                     test: /\.jsx?$/,
                     exclude: /node_modules/,
-                    loader: 'babel-loader',
-                },
-            ],
-        },
-    },
+                    loader: 'babel-loader'
+                }
+            ]
+        }
+    }
 }
